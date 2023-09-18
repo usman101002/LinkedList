abc7329e10bd4c86deec511b04cdcace989cb964
Усман <@DESKTOP-E07M5P8>
2023-09-18 23:05:24 +0700

    small changes

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkedList
+    {
+        private Node head {get; set;}
+
+    }
+}
